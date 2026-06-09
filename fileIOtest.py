d445fb156c43b51b986b28470f01e6cf50d84326
kaioraf <45394248+kaioraf@users.noreply.github.com>
2026-06-09 11:57:51 +0200

    Test file for reading out the .tif data files
    reads out all files in some folder

---
+++ fileIOtest.py
@@ -0,0 +1,22 @@
+import os, glob
+path = '/home/kaio/Documents/Uni/Practicum 1/Data'
+avg_list = {}
+std_dev_list = {}
+for filename in glob.glob(os.path.join(path, '*.csv')):
+      print(filename)
+      with open(os.path.join(os.getcwd(), filename), 'r') as file: # open in readonly mode
+            csvFile = csv.reader(file)
+            next(csvFile)
+            summ = 0
+            len=0
+            for lines in csvFile:
+                  summ += float(lines[1])
+                  len += 1
+            avg_list.update({filename: (summ/len)})
+      with open(os.path.join(os.getcwd(), filename), 'r') as file: 
+            csvFile = csv.reader(file)
+            next(csvFile)
+            var = 0
+            for lines in csvFile:
+                  var += ((float(lines[1]) - (summ/len))**2)
+            s
