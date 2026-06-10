ba66c36d1ea036095b8f9261814fd7bd8f0e2985
TeSomb <98262118+HenkDaSteen@users.noreply.github.com>
2026-06-10 14:48:37 +0200

    Pixelvaluefunction added; test.py emptier
    Pixelvaluefunction is a single file containing a function that turns a specified wattage, voltage and imagecount to the pixelvalue of the first pixel in the correspnding image. It uses other functions from fileIO.
    test.py is a bit emptier: pixelvaluefunction used to reside there

--- pixelvaluefunction.py
+++ pixelvaluefunction.py
@@ -3,12 +3,6 @@ import scipy as sp
 import numpy as np
 from fileIO import *
 import os, glob
-lf4090 = Image = Image.open("2026-06-08_Detector_noise_calibration/90kV/40W/scan_00.tif")
-df00array = np.array(lf4090)
-print(df00array[0, 0])
-dict1=images_to_dict(voltage_type= "90kV/10W")
-a=dict1["c:\\Users\\tesom\\Documents\\GitHub\\Project-group-8\\2026-06-08_Detector_noise_calibration\\90kV/10W\\scan_01.tif"]
-print(a[0, 0])
 
 
 def eerstepixelwaarde(volt, watt, count):
