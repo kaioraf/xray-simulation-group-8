e140e7fa635827026785f9e7f37ea7aa0c783497
TeSomb <98262118+HenkDaSteen@users.noreply.github.com>
2026-06-11 16:27:40 +0200

    Plotjes.py ietsjes netter met functies
    Heb plotjes.py verbeterd en Charlie´s fix voor de snellere functie van Kaio geìmplementeerd: alle plotjes kunnen nu gemaakt worden met een functie ipv ze aan te roepen

--- plotjes.py
+++ plotjes.py
@@ -7,48 +7,53 @@ from fileIO import *
 import matplotlib.pyplot as plt
 
 def pixel_average(voltage_type, x, y):
-    return read_np_image_arrays(voltage_type=voltage_type, dist_type='avg')[x,y]
+    return read_np_image_arrays(voltage_type=voltage_type, dist_type='avg')[y, x]
 
 # print(pixel_average('75kV\\40W', 500, 40))
 
 def pixel_variance(voltage_type, x, y):
-    return read_np_image_arrays(voltage_type=voltage_type, dist_type='var')[x,y]
-
+    return read_np_image_arrays(voltage_type=voltage_type, dist_type='var')[y, x]
 
 voltages = [30, 45, 60, 75, 90]
 wattages = [10, 20, 30, 40]
 
-pixel_x=600
+pixel_x=50
 pixel_y=50
 
-# # #plot van gemiddelde pixelwaarde vs wattage voor voltages
-# for volt in voltages:
-#     averagewattages_lst=[pixel_average(str(volt)+"kV\\"+str(watt)+"W", pixel_x, pixel_y) for watt in wattages]
-#     plt.plot(wattages, averagewattages_lst, marker="o", label=f"{volt} kV")
-#     plt.title("Average pixel value vs wattages for different voltages")
-#     plt.xlabel("Wattages")
-#     plt.ylabel("Averages(geen darkfield afgetrokken)")
-#     plt.legend(["30kV", "45kv", "60kV", "75kV", "90kv"])
-# plt.show()
+#plot van gemiddelde pixelwaarde vs wattage voor voltages
+def plotje_avg_vs_watt():
+    for volt in voltages:
+        averagewattages_lst=[pixel_average(str(volt)+"kV\\"+str(watt)+"W", pixel_x, pixel_y) for watt in wattages]
+        plt.plot(wattages, averagewattages_lst, marker="o", label=f"{volt} kV")
+    plt.title("Average pixel value vs wattages for different voltages")
+    plt.xlabel("Wattages")
+    plt.ylabel("Averages(geen darkfield afgetrokken)")
+    plt.legend(["30kV", "45kv", "60kV", "75kV", "90kv"])
+    plt.show()
 
-# # #plot van variantie vs voltage voor verschillende wattages
-# for watt in wattages:
-#     variancevoltages_lst=[pixel_variance(str(volt)+"kV\\"+str(watt)+"W", pixel_x, pixel_y) for volt in voltages]
-#     plt.plot(voltages, variancevoltages_lst, marker="o")
-#     plt.legend()
-#     plt.title("Variance vs voltage for 4 different wattages")
-#     plt.ylabel("variance (darkfield nog niet eraf gehaald)")
-#     plt.xlabel("voltage")
-#     plt.legend(["10W", "20W", "30W", "40W"])
-# plt.show()
+# #plot van variantie vs voltage voor verschillende wattages
+def plotje_var_vs_volt():
+    for watt in wattages:
+        variancevoltages_lst=[pixel_variance(str(volt)+"kV\\"+str(watt)+"W", pixel_x, pixel_y) for volt in voltages]
+        plt.plot(voltages, variancevoltages_lst, marker="o")
+    plt.legend()
+    plt.title("Variance vs voltage for 4 different wattages")
+    plt.ylabel("variance (darkfield nog niet eraf gehaald)")
+    plt.xlabel("voltage")
+    plt.legend(["10W", "20W", "30W", "40W"])
+    plt.show()
 
 #plot van variantie vs wattage voor verschillende voltages
-for volt in voltages:
-    variancewattages_lst=[pixel_variance(str(volt)+"kV\\"+str(watt)+"W", pixel_x, pixel_y) for watt in wattages]
-    plt.plot(wattages, variancewattages_lst, marker="o")
+def plotje_var_vs_watt():
+    for volt in voltages:
+        variancewattages_lst=[pixel_variance(str(volt)+"kV\\"+str(watt)+"W", pixel_x, pixel_y) for watt in wattages]
+        plt.plot(wattages, variancewattages_lst, marker="o")
     plt.legend()
     plt.title("Variance vs wattage for 5 different voltages")
     plt.ylabel("variance (darkfield nog niet eraf gehaald)")
     plt.xlabel("wattage")
     plt.legend(["30kV", "45kV", "60kV", "75kV", "90kV"])
-plt.show()
+    plt.show()
+
+
+plotje_var_vs_watt()
