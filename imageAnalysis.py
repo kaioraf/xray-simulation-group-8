e746589ccb70c46bd48bdf54215c95faaa1e7d7d
kaioraf <45394248+kaioraf@users.noreply.github.com>
2026-06-12 11:56:30 +0200

    Might have changed the avg_array_darkfield
    might not actually have changed, but i regenerated it to hope to fix an error

--- imageAnalysis.py
+++ imageAnalysis.py
@@ -18,23 +18,21 @@ COUNTS = 20
 def average_full_images(images, voltage_type = 'darkfield', save_file = False):
       
       # get the dimensions of the images
-      height = int(images.shape[0])
-      width = int(images.shape[1])
-      
+      height = int(images.shape[1])
+      width = int(images.shape[0])
       # create an empty array
       avg_array_xy: np.ndarray = np.zeros((width, height))
       
-      for y in range(height - 1):
-            for x in range(width - 1):                   
-                  avg_array_xy[x, y] = average_single_pixel(images = images, x = x, y = y)
-
+      for y in range(height):
+            for x in range(width):                   
+                  avg_array_xy[x, y] = average_single_pixel(images, x, y)
       if save_file:
             dirname: str = os.path.dirname(p = __file__)
             # remove the slash from the filename
             safe_path: str = voltage_type[:4] + voltage_type[5:]
             # save values to .npy file to read out the image: avg_image = np.load("avg_array_75kV_10W.npy")
             np.save(file = f"{dirname}/Numpy image arrays/{voltage_type}/avg_array_{safe_path}.npy", arr = avg_array_xy) # macos/linux
-
+            print(voltage_type, safe_path)
       return avg_array_xy
 
 # take all the files within some folder e.g. 75kV/10W/, and for each pixel calculate the variance of the 20 images
@@ -42,18 +40,17 @@ def average_full_images(images, voltage_type = 'darkfield', save_file = False):
 def variance_full_images(images, voltage_type = 'darkfield', save_file = False):
 
       # get dimension of the images
-      height = int(images.shape[0])
-      width = int(images.shape[1])
-
+      height = int(images.shape[1])
+      width = int(images.shape[0])
       # create an empty array
       var_array_xy: np.ndarray = np.zeros((width, height))
 
       for y in range(height - 1):
             for x in range(width - 1):
-                  var_array_xy[x, y] = get_variance_single_pixel(images = images, x = x, y = y)
+                  var_array_xy[x, y] = get_variance_single_pixel(images, x, y)
       
       if save_file:
-            dirname: str = os.path.dirname(p = __file__)
+            dirname: str = os.path.dirname(__file__)
             # remove the slash from the filename
             safe_path: str = voltage_type[:4] + voltage_type[5:]
             # save values to .npy file to read out the image: var_image = np.load("var_array_75kV_10W.npy")
@@ -63,19 +60,19 @@ def variance_full_images(images, voltage_type = 'darkfield', save_file = False):
 
 # take the stack of 20 images and average their values
 def average_single_pixel(images, x, y):
-      return np.average(images[y, x])
+      return np.average(images[x, y])
       
 def get_variance_single_pixel(images, x, y):
-      return np.var(images[y, x])
+      return np.var(images[x, y])
 
 # go from an image array to a .png, use exposure to adjust the brightness
 def create_image(image, exposure = 1, filename = 'result.png'):
       print(filename)
       image: Image = image * exposure
-      picture: Image = Image.fromarray(obj = (image).astype(np.uint16))
+      picture: Image = Image.fromarray(obj = (image.transpose()).astype(np.uint16))
       picture.save(filename)
       
-def create_all_images(): # very long function, do not run if the files are already created!
+def create_flatfield_images(): # very long function, do not run if the files are already created!
       for voltage in VOLTAGES:
             for wattage in WATTAGES:
                   print(voltage, wattage)
@@ -90,7 +87,22 @@ def create_all_images(): # very long function, do not run if the files are alrea
                   create_image(image = avg, filename = full_path)
                   full_path = f"{dirname}/Numpy image arrays/{path}/var_array_{safe_path}.png"
                   create_image(image = var, filename = full_path)
-
+            
+def create_darkfield_images():
+      path: str = 'darkfield'
+      images: np.ndarray = images_to_array(voltage_type = path)
+      avg: np.ndarray = average_full_images(images = images, voltage_type = path, save_file = True)
+      var: np.ndarray = variance_full_images(images = images, voltage_type = path, save_file = True)
+      
+      dirname: str = os.path.dirname(p = __file__)
+      safe_path: str = path[:4] + path[5:]
+      full_path: str = f"{dirname}/Numpy image arrays/{path}/avg_array_{safe_path}.png"
+      create_image(image = avg, filename = full_path)
+      full_path = f"{dirname}/Numpy image arrays/{path}/var_array_{safe_path}.png"
+      create_image(image = var, filename = full_path)
+      
+create_darkfield_images()
+# create_image(average_full_images(images_to_array()))
 # start: float = time.perf_counter() # timer
 # create_all_images()
 # image: np.ndarray = images_to_array()
